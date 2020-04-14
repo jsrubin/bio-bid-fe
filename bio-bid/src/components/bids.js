71d40208962a00cfed120027519c8ffd306d81da
Andrea Harris <andrea@andreas-imac.lan>
2020-04-14 09:53:07 -0500

    nav links modification

--- bio-bid/src/components/bids.js
+++ bio-bid/src/components/bids.js
@@ -1,16 +1,17 @@
 import React from "react";
-import styled from "styled-components";
 import MaterialTable from "material-table";
 
 export default function Table() {
   const [state, setState] = React.useState({
     columns: [
+        { title: "BIDS", field: "bids" },
       { title: "NAME", field: "name" },
       { title: "THERAPEUTIC AREA", field: "Therapeutic" },
       { title: "PROTOCOL NO./TITLE", field: "protocol", type: "numeric" },
       { title: "PHASE", field: "Phase"},
       { title: "SERVICE LIST" , field: "Serverlist", type: "numeric" },
       { title: "MODIFIED LIST", field: "modify", type: "numeric" },
+      {title: "ACTIONS", field: "actions"},
       {
         title: "",
         field: "",
@@ -24,7 +25,134 @@ export default function Table() {
         protocol: "ABZ-123",
         Phase: "I",
         Serverlist: 1,
-        modify: "April 19 ,2019"
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
+      },
+      {
+        name: "Second study",
+        Therapeutic: "Inflammation",
+        protocol: "ABZ-123",
+        Phase: "I",
+        Serverlist: 1,
+        modify: "April 19 ,2019",
+        actions: ""
       },
     ]
   });
--- bio-bid/src/components/navigation.js
+++ bio-bid/src/components/navigation.js
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
+import {PieChartOutlined}from "@ant-design/icons";
+import {TeamOutlined} from "@ant-design/icons";
+import {FileSearchOutlined} from "@ant-design/icons";
+import {QrcodeOutlined} from "@ant-design/icons";
+import {SettingOutlined} from "@ant-design/icons";
 
 const Nav =() =>{
     return(
@@ -20,38 +25,43 @@ const Nav =() =>{
                 
 
                 <div className="navWrapperDiv">
-                    <StyledLink to="/projects">
+                    <StyledLink to="/projects" style={{margin:'15%'}}>
+                    <QrcodeOutlined/>
                         Current Projects
                     </StyledLink>
                 </div>
 
                 <div className="navWrapperDiv">
-                    <StyledLink to="/bids">
+                    <StyledLink to="/bids" style={{margin:'17%'}}>
+                    <PieChartOutlined/>
                         Manage Bids
                     </StyledLink>
                 </div>
 
                 <div className="navWrapperDiv">
-                    <StyledLink to="/recommendations">
+                    <StyledLink to="/recommendations" style={{margin:'14%', padding:'2%'}}>
+                    <TeamOutlined/>
                     Recommendations
                     </StyledLink>
                 </div>
 
                 <div className="navWrapperDiv">
-                    <StyledLink to="/search">
+                    <StyledLink to="/search" style={{margin:'17%'}}>
+                    <FileSearchOutlined />
                         Search studies
                     </StyledLink>
                 </div>
 
-                <div className="navWrapperDiv">
-                    <StyledLink to="/settings">
+                <div className="navWrapperDiv2">
+                    <StyledLink2 to="/settings" style={{margin:'17%'}}>
+                    <SettingOutlined/>
                         Settings
-                    </StyledLink>
+                    </StyledLink2>
                 </div>
-                <div className="navWrapperDiv">
-                    <StyledLink to="/logout">
+                <div className="navWrapperDiv2">
+                    <StyledLink2 to="/logout" style={{margin:'17%'}}>
                         Log out
-                    </StyledLink>
+                    </StyledLink2>
                 </div>
             </div>     
 
@@ -65,8 +75,9 @@ const NavWrapper = styled.div
 `
 
 background: white;
-width: 17%;
+width: 20%;
 height:100vh;
+
 `
 const StyledLink =styled.div
 `
@@ -76,7 +87,7 @@ color: #595959;
 font-size: 22px;
 height:10%;
 text-decoration:none;
-margin: 10% 5%;
+margin: 5% 0 0 5%;
 &: hover{
     background: #C4C4C4;
     border-radius: 0px 20px 20px 0px;
@@ -98,17 +109,21 @@ margin: 15% 5% 20% 5%;
 line-height: 45%;
 `
 
-const StyledOut =styled.div
+const StyledLink2 =styled.div
 `
 font-family: Lato;
 font-style: normal;
 font-weight: normal;
 font-size: 22px;
-line-height: 60%;
-color: #21242C;
-margin:600px 0 10% 5%;
+color: #595959;
+border-top: 1 px solid gray;
+
+
+
+
 
 `
+
 
 export default Nav;
 
