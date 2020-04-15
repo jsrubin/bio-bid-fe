a52384b57cbb13a65ff970ec21da49da4a2ee6a9
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-04-15 11:13:37 -0500

    added link to backend for apolloclient

--- bio-bid/src/index.js
+++ bio-bid/src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
+const client = new ApolloClient({uri: 'http://ec2-34-195-186-223.compute-1.amazonaws.com/'})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App/>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
