34a8bdb8501fc5c89385d4bc80daa62e2a51f26c
Josiah Roa <josiahroa18@gmail.com>
2020-05-18 11:11:36 -0600

    Fixing npm and yarn issue

--- bio-bid/src/components/company-profile/Form/Form.js
+++ bio-bid/src/components/company-profile/Form/Form.js
@@ -4,7 +4,26 @@ import { Form } from './styles';
 export default props => {
     return (
         <Form>
-            <h1>Form</h1>
+            <div className='body'>
+                <div className='header-wrapper'>
+                    {props.edit ? (
+                        <h1>Edit company profile</h1>
+                    ) : (
+                        <h1>Add company profile</h1>
+                    )}
+                    <div className='btn-container'>
+                        <div className='btn linkedin'>
+                            <p>Import</p>
+                        </div>          
+                        <div className='btn cancel'>
+                            <p>Cancel</p>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                <div className='bar'/>
+            </div>
         </Form>
     );
 }
--- bio-bid/src/components/company-profile/Form/styles.js
+++ bio-bid/src/components/company-profile/Form/styles.js
@@ -1,8 +1,30 @@
 import styled from 'styled-components';
 import theme from '../../../theme';
+import { LinkedinSquare } from '@styled-icons/boxicons-logos'
 
 export const Form = styled.form`
-    h1{
-        font: ${theme.fontStyling.text};
+    display: flex;
+    width: 100%;
+    .body{
+        margin: 0 auto;
+        width: 80%;
+        .header-wrapper{
+            display: flex;
+            justify-content: space-between;
+            h1{
+                margin-top: 50px;
+                font: ${theme.fontStyling.header3};
+            }
+            .btn-container{
+
+            }
+        }
+        .bar{
+            width: 100%;
+            height: 1px;
+            background-color: ${theme.colors.scienceBlue};
+        }
     }
 `;
+
+export const LinkedIn = styled(LinkedinSquare);
