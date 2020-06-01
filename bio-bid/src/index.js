643a4df4089b33595212309552842db03fb861b7
Nobro777 <52586100+Nobro777@users.noreply.github.com>
2020-06-01 07:47:43 -0700

    company list working

--- bio-bid/src/index.js
+++ bio-bid/src/index.js
@@ -13,12 +13,12 @@ import { gql } from 'apollo-boost';
 // http://biobidbe-env.eba-ercbzmhq.us-east-1.elasticbeanstalk.com/
 // https://ec2-34-195-186-223.compute-1.amazonaws.com/
 const client = new ApolloClient({
-    uri: process.env.REACT_APP_SERVER_ENDPOINT || 'https://us1.prisma.sh/biobid-team/production/prod/',
+    uri: process.env.REACT_APP_SERVER_ENDPOINT || 'https://us1.prisma.sh/biobid-team/production/prod',
     typeDefs: gql`
       enum CompanySize {
         A, B, C, D, E, F, G, H, I
       }
-    `,
+    `
 })
 
 //'https://us1.prisma.sh/biobid-team/production/prod'
