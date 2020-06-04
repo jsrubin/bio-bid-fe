52c722744ddd00c7ff97ac5658933173aad8c868
Gavin S <stahlgavin@gmail.com>
2020-06-04 10:43:32 -0700

    config

--- bio-bid/src/index.js
+++ bio-bid/src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { gql } from 'apollo-boost';
-import * as FullStory from '@fullstory/browser';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router } from "react-router-dom";
+import { gql } from "apollo-boost";
+import * as FullStory from "@fullstory/browser";
+import {Security} from "@okta/okta-react";
 
 const client = new ApolloClient({
-
-  uri: process.env.REACT_APP_SERVER_ENDPOINT || 'https://api.biobidlabs.com/',
+  uri: process.env.REACT_APP_SERVER_ENDPOINT || "https://api.biobidlabs.com/",
   typeDefs: gql`
     enum CompanySize {
       A
@@ -30,15 +30,25 @@ const client = new ApolloClient({
   `,
 });
 
-FullStory.init({ orgId: 'VHMTY' });
+const config = {
+  // for okta-react components
+  issuer: "https://dev-648803.okta.com/oauth2/default",
+  clientId: "0oadk3f256MfKqlA74x6",
+  redirectUri: `${window.location.origin}/service-providers`,
+  pkce: true,
+};
+
+FullStory.init({ orgId: "VHMTY" });
 
 ReactDOM.render(
   <Router>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <Security {...config}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Security>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
